f5624ffc1e598b845245d1f94a546564f29bb3e6
alveescan <prax1951@gmail.com>
2026-03-26 21:50:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,3 +164,13 @@ client.on("messageDelete", (message) => {
   });
 });
 client.login(process.env.TOKEN);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot aktif 🔥");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server çalışıyor");
+});
